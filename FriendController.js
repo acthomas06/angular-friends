30daee608cd265adb9433ad13fa2a66883340629
Andrew Thomas <acthomas06@github.com>
2014-01-30 18:13:07 -0700

    first commit

--- FriendController.js
+++ FriendController.js
@@ -1,4 +1,20 @@
+app.controller('FriendController', function($scope, $http){
+	$http.get('https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json').success(function(data) {
+		$scope.friends = data.results;
+		angular.forEach($scope.friends, function(detail){
+			detail.friend_count = parseInt(detail.friend_count, 10);
+		});
+		console.log($scope.friends);
+	});
 
+	$scope.searchTerm = {
+		name: '',
+		current_location: ''
+	}
+
+	$scope.searchSort = 'name';
+	$scope.sortAZ = false;
+});
 
 
 
